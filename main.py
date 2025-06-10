b64d7757f97e59d4f3e953ef48030fe3ef64b51e
VMT1312 <thiemmavinh123@gmail.com>
2025-06-10 18:10:42 +0800

    Add initial project files: .gitignore, main.py, and requirements.txt

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+res = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+
+print(res.text)
+
+prompt_token_count = res.usage_metadata.prompt_token_count
+candidates_token_count = res.usage_metadata.candidates_token_count
+
+print(f"Prompt tokens: {prompt_token_count}\nResponse tokens: {candidates_token_count}")
