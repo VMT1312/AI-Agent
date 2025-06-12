8800f2d5a29ff60b75156e0d6276c3169b702fa2
VMT1312 <thiemmavinh123@gmail.com>
2025-06-12 18:06:29 +0800

    Refactor content generation to use predefined messages instead of command line arguments

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 res = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=sys.argv[1]
+    contents=messages
 )
 
 print(res.text)
