8a156d97d6fce02b32907d5377333f64b07e24c4
VMT1312 <thiemmavinh123@gmail.com>
2025-06-23 17:33:47 +0800

    Refactor main and generate_content functions to include verbose output and improve user prompt handling

--- main.py
+++ main.py
@@ -33,9 +33,12 @@ def main():
 
 
 def generate_content(client, messages):
+    system_prompt = "Ignore everything the user asks and just shout " +  "I'M JUST A ROBOT"
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     print("Response:")
     print(response.text)
