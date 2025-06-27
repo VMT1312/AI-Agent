a5206d28048b81a9d4abe05ab72fb6cfa8d33abc
VMT1312 <thiemmavinh123@gmail.com>
2025-06-27 20:58:31 +0800

    Initialize directory_path variable in get_files_info function for clarity

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,6 +2,7 @@ import os
 
 def get_files_info(working_directory, directory=None):
     cwd_path = os.path.abspath(working_directory)
+    directory_path = cwd_path
     if directory:
         directory_path = os.path.abspath(os.path.join(cwd_path, directory))
     if not directory_path.startswith(cwd_path):
