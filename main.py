86a54f6a71a7b22d22aa464a498fe44104af5ad3
VMT1312 <thiemmavinh123@gmail.com>
2025-06-27 20:58:10 +0800

    Refactor main function and enhance generate_content to support verbose logging and function call handling

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ from dotenv import load_dotenv
 from prompts import system_prompt
 from declarations import *
 
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+
+function_dict = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+
 
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant")
@@ -23,27 +37,35 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if "--verbose" in user_prompt:
+        print(f"User prompt: {user_prompt}")
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    prompt_tokens, response_tokens = generate_content(client, messages)
+    for i in range(20):
+        response, function_call_results = generate_content(client, messages, verbose)
+        if not response.function_calls:
+            print(response.text)
+            break
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+        messages.extend(function_call_results)
 
-    if "--verbose" in user_prompt:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
 
-
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     available_functions = types.Tool(
         function_declarations=[
             schema_get_files_info,
             schema_get_file_content,
-            scheme_run_python_file,
+            schema_run_python_file,
             schema_write_file,
         ]
     )
+
+    function_call_results = []
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -53,21 +75,62 @@ def generate_content(client, messages):
         ),
     )
 
-    function_call_part = response.function_calls
-
-    if function_call_part:
-        print("Function calls:")
-        print(f"Calling function: {function_call_part[0].name}({function_call_part[0].args})")
-    else:
-        print("Response:")
-        print(response.text)
-
-
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
 
-    return (prompt_tokens, response_tokens)
+    if verbose:
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
+    function_calls = response.function_calls
+
+    if not function_calls:
+        return response, None
+
+    for function_call in function_calls:
+        function_call_result = call_function(function_call, verbose)
+        function_call_results.append(function_call_result)
+        if "error" not in function_call_result.parts[0].function_response.response:
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            raise Exception(
+                f"Function {function_call.name} failed with error: {function_call_result.parts[0].function_response.response['error']}"
+            )
+
+    return response, function_call_results
+
+
+def call_function(function_call_part: types.FunctionCall, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    args = function_call_part.args
+
+    try:
+        function_result = function_dict[function_name](**{"working_directory": "./calculator", **args})
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+    except KeyError:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"}
+                )
+            ]
+        )
 
 if __name__ == "__main__":
     main()
